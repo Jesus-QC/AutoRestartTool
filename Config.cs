feb032fb312938c058f6968ad235f7b613281027
xRoier <58123692+xRoier@users.noreply.github.com>
2020-11-01 01:06:35 +0100

    Fixed some typo.

--- Config.cs
+++ Config.cs
@@ -7,10 +7,8 @@ namespace ART
     {
         [Description("Is the plugin enabled? **YOU NEED MULTIADMIN**")]
         public bool IsEnabled { get; set; } = true;
-
         [Description("Rounds to restart the server (0 to restart the server every round)[-1 to disable it]")]
         public int RoundsNeededToRestart { get; set; } = 50;
-
         [Description("At this time server will restart every day (Example: 23:59) [-1 to disable it]")]
         public string TimeOfRestart { get; set; } = "23:59";
     }
