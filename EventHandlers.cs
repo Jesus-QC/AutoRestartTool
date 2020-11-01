6a7938b209a51e13ffac4200e6a257b595ef293c
xRoier <58123692+xRoier@users.noreply.github.com>
2020-11-01 01:05:07 +0100

    Fixed some typo.

--- EventHandlers.cs
+++ EventHandlers.cs
@@ -1,41 +1,34 @@
-﻿using System;
-using MEC;
-using System.Diagnostics;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using Exiled.API.Features;
+using MEC;
 
-namespace ART
+namespace AutoRestartTool_xRoier
 {
     internal class EventHandlers
     {
         private readonly ART plugin;
         public EventHandlers(ART plugin) => this.plugin = plugin;
-
         public int Rounds;
-
         public void OnRestarting()
         {
             Rounds++;
-            if (plugin.Config.TimeOfRestart != "-1") { Timing.RunCoroutine(TimeChecker()); }
+            if (plugin.Config.TimeOfRestart != "-1") 
+                Timing.RunCoroutine(TimeChecker());
             if (Rounds >= plugin.Config.RoundsNeededToRestart && plugin.Config.RoundsNeededToRestart > -1)
-            {
-                Timing.CallDelayed(1.5f, () => { Process.GetCurrentProcess().Kill(); });
-            }
+                Timing.CallDelayed(1.5f, () => Process.GetCurrentProcess().Kill());
         }
-
         IEnumerator<float> TimeChecker()
         {
-            for (; ; )
+            for(;;)
             {
                 if (DateTime.Now.ToShortTimeString() == plugin.Config.TimeOfRestart)
                 {
                     Round.Restart();
-                    Timing.CallDelayed(1.5f, () => { Process.GetCurrentProcess().Kill(); });
+                    Timing.CallDelayed(1.5f, () => Process.GetCurrentProcess().Kill());
                 }
-                else
-                {
-                    yield return Timing.WaitForSeconds(60);
-                }
+                yield return Timing.WaitForSeconds(60);
             }
         }
     }
